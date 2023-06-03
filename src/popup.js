c25679667a4aa60edb0288feda5bcb97ffe1648a
Vansh Sukhija <12112021@nitkkr.ac.in>
2023-06-04 00:39:17 +0530

    added rating-wise potd and updated popup

--- src/popup.js
+++ src/popup.js
@@ -3,6 +3,44 @@ let output = document.getElementById('output')
 let probstats = [];
 let probs = [];
 let probind = 0;
+let Rprobstats = [];
+let Rprobs = [];
+let Rprobind = 0;
+let userrating = 800;
+let username = "Enter";
+
+chrome.storage.local.get("rating", function (res) {
+    userrating = res.rating;
+});
+chrome.storage.local.get("name", function (res) {
+    username = res.name;
+});
+
+window.onload = async () => {
+    await fetch(`https://codeforces.com/api/problemset.problems`)
+        .then(response => response.json())
+        .then(data => {
+            let recentContestId = data.result.problems[0].contestId;
+            for (let i = 0; i < data.result.problems.length; i++) {
+                if (('rating' in data.result.problems[i])) {
+                    recentContestId = data.result.problems[i].contestId;
+                    break;
+                }
+            }
+
+            for (let i = 0; i < data.result.problems.length && data.result.problems[i].contestId >= recentContestId - 750; i++) {
+                if (('rating' in data.result.problems[i]) && (data.result.problems[i].contestId >= recentContestId - 500) && (data.result.problems[i].contestId <= recentContestId - 100)) {
+                    probstats.push(data.result.problemStatistics[i]);
+                    probs.push(data.result.problems[i])
+                }
+                if ('rating' in data.result.problems[i] && Math.abs(data.result.problems[i].rating - userrating) <= 200) {
+                    Rprobstats.push(data.result.problemStatistics[i]);
+                    Rprobs.push(data.result.problems[i])
+                }
+            }
+        })
+        .catch(err => console.log(err))
+}
 
 cal.addEventListener('change', async () => {
     output.innerHTML = `Loading...`
@@ -19,23 +57,23 @@ cal.addEventListener('change', async () => {
     if (cal.value > today)
         output.innerHTML = "You do have a time machine, don't you?"
     else {
-        await fetch(`https://codeforces.com/api/problemset.problems`)
-        .then(response => response.json())
-        .then(data => {
-            for (let i = 0; i < data.result.problems.length; i++) {
-                if (('rating' in data.result.problems[i]) && data.result.problems[i].rating <= 2000 && data.result.problems[i].rating >= 1000) {
-                    probstats.push(data.result.problemStatistics[i]);
-                    probs.push(data.result.problems[i])
-                }
-            }
-        })
-        .catch(err => console.log(err))
-
         date = parseInt(cal.value.split('-')[2])
         month = parseInt(cal.value.split('-')[1])
         year = parseInt(cal.value.split('-')[0])
         probind = (year + month * date + 1709) % probs.length
+        Rprobind = (year + month * date + 1709) % Rprobs.length
 
-        output.innerHTML = `<a href="https://codeforces.com/problemset/problem/${probs[probind].contestId}/${probs[probind].index}" target="_blank">${probs[probind].name}</a>`
+        output.innerHTML = `
+        <div class="ques">
+                <div>POTD-G</div>
+                <a href="https://codeforces.com/problemset/problem/${probs[probind].contestId}/${probs[probind].index}" target="_blank">${probs[probind].name}</a>
+        </div>`
+        if (username != "Enter") {
+            output.innerHTML += `
+            <div class="ques">
+                <div>POTD-R</div>
+                <a href="https://codeforces.com/problemset/problem/${Rprobs[Rprobind].contestId}/${Rprobs[Rprobind].index}" target="_blank">${Rprobs[Rprobind].name}</a>
+            </div>`
+        }
     }
 })
--- src/script.js
+++ src/script.js
@@ -15,26 +15,73 @@ if (!document.URL.includes('acmsguru')) {
     let cell4 = row1.insertCell(3);
     let cell5 = row1.insertCell(4);
 
-    cell1.innerText = 'POTD'
+    cell1.innerText = 'POTD-G'
     cell2.innerText = 'Loading...'
     cell3.innerText = 'Loading...'
     cell4.innerText = 'Loading...'
     cell5.innerText = 'Loading...'
+    
+    let row2
+    let Rcell1
+    let Rcell2
+    let Rcell3
+    let Rcell4
+    let Rcell5
 
     let username = document.getElementsByClassName('lang-chooser')[0].childNodes[3].childNodes[1].innerText
+    chrome.storage.local.set({"name": username});
+    if(username != "Enter"){
+        row2 = table.insertRow(2);
+        Rcell1 = row2.insertCell(0);
+        Rcell2 = row2.insertCell(1);
+        Rcell3 = row2.insertCell(2);
+        Rcell4 = row2.insertCell(3);
+        Rcell5 = row2.insertCell(4);
+        
+        Rcell1.innerText = 'POTD-R'
+        Rcell2.innerText = 'Loading...'
+        Rcell3.innerText = 'Loading...'
+        Rcell4.innerText = 'Loading...'
+        Rcell5.innerText = 'Loading...'
+    }
 
+    let userrating = 800;
     let probstats = [];
     let probs = [];
     let probind = 0;
+    let Rprobstats = [];
+    let Rprobs = [];
+    let Rprobind = 0;
+
     async function getData() {
+        await fetch(`https://codeforces.com/api/user.rating?handle=${username}`)
+            .then(response => response.json())
+            .then(data => {
+                if (data.result.length > 0)
+                    userrating = Math.max(data.result[data.result.length - 1].newRating, userrating);
+                userrating = 100*Math.round(userrating/100)
+                chrome.storage.local.set({"rating": userrating});
+            })
+
         await fetch(`https://codeforces.com/api/problemset.problems`)
             .then(response => response.json())
             .then(data => {
                 let recentContestId = data.result.problems[0].contestId;
                 for (let i = 0; i < data.result.problems.length; i++) {
-                    if (('rating' in data.result.problems[i]) && data.result.problems[i].rating <= 2200 && data.result.problems[i].rating >= 1000 && data.result.problems[i].contestId >= recentContestId-500) {
+                    if (('rating' in data.result.problems[i])) {
+                        recentContestId = data.result.problems[i].contestId;
+                        break;
+                    }
+                }
+
+                for (let i = 0; i < data.result.problems.length && data.result.problems[i].contestId >= recentContestId - 750; i++) {
+                    if (('rating' in data.result.problems[i]) && (data.result.problems[i].contestId >= recentContestId - 500) && (data.result.problems[i].contestId <= recentContestId - 100)) {
                         probstats.push(data.result.problemStatistics[i]);
                         probs.push(data.result.problems[i])
+                    }
+                    if ('rating' in data.result.problems[i] && Math.abs(data.result.problems[i].rating - userrating) <= 200) {
+                        Rprobstats.push(data.result.problemStatistics[i]);
+                        Rprobs.push(data.result.problems[i])
                     }
                 }
             })
@@ -45,6 +92,7 @@ if (!document.URL.includes('acmsguru')) {
         let month = today.getMonth() + 1;
         let date = today.getDate();
         probind = (year + month * date + 1709) % probs.length
+        Rprobind = (year + month * date + 1709) % Rprobs.length
 
         cell1.innerHTML = data1.innerHTML
         cell2.innerHTML = data2.innerHTML
@@ -52,28 +100,57 @@ if (!document.URL.includes('acmsguru')) {
         cell4.innerHTML = data4.innerHTML
         cell5.innerHTML = data5.innerHTML
 
-        row1.childNodes[0].innerHTML = `<a href="/problemset/problem/${probs[probind].contestId}/${probs[probind].index}">POTD</a>`
+        cell1.innerHTML = `<a href="/problemset/problem/${probs[probind].contestId}/${probs[probind].index}">POTD-G</a>`
 
-        row1.childNodes[1].childNodes[1].innerHTML = `<a href="/problemset/problem/${probs[probind].contestId}/${probs[probind].index}">${probs[probind].name}</a>`
-
-        row1.childNodes[1].childNodes[3].innerHTML = "";
+        cell2.childNodes[1].innerHTML = `<a href="/problemset/problem/${probs[probind].contestId}/${probs[probind].index}">${probs[probind].name}</a>`
+        
+        cell2.childNodes[3].innerHTML = "";
         for (let i = 0; i < probs[probind].tags.length; i++) {
             let taglink = probs[probind].tags[i].replaceAll(' ', '+');
 
-            row1.childNodes[1].childNodes[3].innerHTML += `<a href="/problemset?tags=${taglink}" style="text-decoration: none;" class="notice">${probs[probind].tags[i]}</a>`
-
+            cell2.childNodes[3].innerHTML += `<a href="/problemset?tags=${taglink}" style="text-decoration: none;" class="notice">${probs[probind].tags[i]}</a>`
+            
             if (i != probs[probind].tags.length - 1)
-                row1.childNodes[1].childNodes[3].innerHTML += ", "
+                cell2.childNodes[3].innerHTML += ", "
         }
 
-        row1.childNodes[2].childNodes[1].innerHTML = `<a href="/problemset/submit/${probs[probind].contestId}/${probs[probind].index}"><img src="https://codeforces.org/s/47998/images/icons/submit-22x22.png" title="Submit" alt="Submit"></a>`
+        cell3.childNodes[1].innerHTML = `<a href="/problemset/submit/${probs[probind].contestId}/${probs[probind].index}"><img src="https://codeforces.org/s/47998/images/icons/submit-22x22.png" title="Submit" alt="Submit"></a>`
 
-        row1.childNodes[2].removeChild(row1.childNodes[2].lastElementChild)
+        cell3.removeChild(cell3.lastElementChild)
+        
+        cell4.innerHTML = `<span title="Difficulty" class="ProblemRating">${probs[probind].rating}</span>`
+        
+        cell5.innerHTML = `<a title="Participants solved the problem" href="/problemset/status/${probs[probind].contestId}/problem/${probs[probind].index}" style="font-size: 1.1rem"><img style="vertical-align:middle;" src="https://codeforces.org/s/47998/images/icons/user.png">&nbsp;x${probstats[probind].solvedCount}</a>`
+        
+        if(username != "Enter"){
+            Rcell1.innerHTML = data1.innerHTML
+            Rcell2.innerHTML = data2.innerHTML
+            Rcell3.innerHTML = data3.innerHTML
+            Rcell4.innerHTML = data4.innerHTML
+            Rcell5.innerHTML = data5.innerHTML
 
-        row1.childNodes[3].innerHTML = `<span title="Difficulty" class="ProblemRating">${probs[probind].rating}</span>`
+            Rcell1.innerHTML = `<a href="/problemset/problem/${Rprobs[Rprobind].contestId}/${Rprobs[Rprobind].index}">POTD-R</a>`
 
-        row1.childNodes[4].innerHTML = `<a title="Participants solved the problem" href="/problemset/status/${probs[probind].contestId}/problem/${probs[probind].index}" style="font-size: 1.1rem"><img style="vertical-align:middle;" src="https://codeforces.org/s/47998/images/icons/user.png">&nbsp;x${probstats[probind].solvedCount}</a>`
+            Rcell2.childNodes[1].innerHTML = `<a href="/problemset/problem/${Rprobs[Rprobind].contestId}/${Rprobs[Rprobind].index}">${Rprobs[Rprobind].name}</a>`
 
+            Rcell2.childNodes[3].innerHTML = "";
+            for (let i = 0; i < Rprobs[Rprobind].tags.length; i++) {
+                let taglink = Rprobs[Rprobind].tags[i].replaceAll(' ', '+');
+    
+                Rcell2.childNodes[3].innerHTML += `<a href="/problemset?tags=${taglink}" style="text-decoration: none;" class="notice">${Rprobs[Rprobind].tags[i]}</a>`
+    
+                if (i != Rprobs[Rprobind].tags.length - 1)
+                    Rcell2.childNodes[3].innerHTML += ", "
+            }
+
+            Rcell3.childNodes[1].innerHTML = `<a href="/problemset/submit/${Rprobs[Rprobind].contestId}/${Rprobs[Rprobind].index}"><img src="https://codeforces.org/s/47998/images/icons/submit-22x22.png" title="Submit" alt="Submit"></a>`
+            Rcell3.removeChild(Rcell3.lastElementChild)
+
+            Rcell4.innerHTML = `<span title="Difficulty" class="ProblemRating">${Rprobs[Rprobind].rating}</span>`
+
+            Rcell5.innerHTML = `<a title="Participants solved the problem" href="/problemset/status/${Rprobs[Rprobind].contestId}/problem/${Rprobs[Rprobind].index}" style="font-size: 1.1rem"><img style="vertical-align:middle;" src="https://codeforces.org/s/47998/images/icons/user.png">&nbsp;x${Rprobstats[Rprobind].solvedCount}</a>`
+        }
+        
         getProbStatus();
     }
     getData();
@@ -82,21 +159,28 @@ if (!document.URL.includes('acmsguru')) {
         await fetch(`https://codeforces.com/api/user.status?handle=${username}`)
             .then(response => response.json())
             .then(data => {
-                let flag = 0;
+                let flagG = 0, flagR = 0;
                 for (let i = 0; i < data.result.length; i++) {
                     if (data.result[i].problem.contestId == probs[probind].contestId && data.result[i].problem.index == probs[probind].index) {
-                        if (data.result[i].verdict == "OK")
-                            flag = 1;
-                        else
-                            flag = -1;
+                        if (data.result[i].verdict == "OK") flagG = 1;
+                        else flagG = -1;
+                        break;
+                    }
+                    if (username != "Enter" && data.result[i].problem.contestId == Rprobs[Rprobind].contestId && data.result[i].problem.index == Rprobs[Rprobind].index) {
+                        if (data.result[i].verdict == "OK") flagR = 1;
+                        else flagR = -1;
                         break;
                     }
                 }
-                if (flag == 1) {
-                    document.getElementsByClassName('problems')[0].childNodes[1].childNodes[2].classList.add('accepted');
+                if (flagG == 1) {
+                    row1.classList.add('accepted');
+                } else if (flagG == -1) {
+                    row1.classList.add('rejected');
                 }
-                else if (flag == -1) {
-                    document.getElementsByClassName('problems')[0].childNodes[1].childNodes[2].classList.add('rejected');
+                if (flagR == 1) {
+                    row2.classList.add('accepted');
+                } else if (flagR == -1) {
+                    row2.classList.add('rejected');
                 }
             })
             .catch(err => console.log(err));
